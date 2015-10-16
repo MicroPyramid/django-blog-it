45ed94290ab739bbb7fcb1291afdb4b9e1145434
chaitanya <chaitanya@micropyramid.com>
2015-10-16 13:30:21 +0530

    changed path of blog folders

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 import os
 from setuptools import setup
+from setuptools import setup, find_packages
 
 with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
     README = readme.read()
@@ -22,8 +23,8 @@ for dirpath, dirnames, filenames in os.walk(PROJECT_NAME):
 
 setup(
     name='micro-blog',
-    version='0.0.2',
-    packages=['microblog', 'microblog.migrations', 'posts.migrations', 'posts.templatetags'],
+    version='0.0.3',
+    packages=find_packages(exclude=['tests', 'tests.*']),
     include_package_data=True,
     description='A simple installable app for writing blog posts',
     long_description=README,
