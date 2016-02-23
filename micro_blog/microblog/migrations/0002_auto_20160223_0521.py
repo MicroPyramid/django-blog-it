ae76a67e7e6876bbbee83e901a91a44b2e48d392
Shirisha Gaddi <sirisha.gaddi@micropyramid.com>
2016-02-23 10:54:51 +0530

    Changed tags text-field to many-to-many field in blog post model.

---
+++ micro_blog/microblog/migrations/0002_auto_20160223_0521.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('microblog', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='post',
+            name='tags',
+        ),
+        migrations.AddField(
+            model_name='post',
+            name='tags',
+            field=models.ManyToManyField(related_name='rel_posts', to='microblog.Tags'),
+        ),
+    ]
