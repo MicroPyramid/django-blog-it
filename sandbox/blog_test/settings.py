32d41481a0f4849dded30daa0fa443eeb7ff6d23
chaitu <chaitanya@micropyramid.com>
2016-05-27 17:20:06 +0530

    updated database settings

--- sandbox/blog_test/settings.py
+++ sandbox/blog_test/settings.py
@@ -77,12 +77,10 @@ WSGI_APPLICATION = 'blog_test.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+   'default': dj_database_url.config(
+       default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
+   )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
